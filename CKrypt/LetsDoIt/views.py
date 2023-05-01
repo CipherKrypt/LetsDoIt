b15cd1e8f5b38dcab15dd76233c98a570cfc08d9
CipherKrypt <SS4334@live.mdx.ac.uk>
2023-05-01 22:54:27 +0400

    add task, format task and sessions

--- CKrypt/LetsDoIt/views.py
+++ CKrypt/LetsDoIt/views.py
@@ -1,10 +1,36 @@
 from django.shortcuts import render
+from django import forms
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
-tasks = ["task1", "task2", "task3"]
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label = "New Task")
+    description = forms.CharField(label = "Description")
+
 def index(request):
+    if "tasks" not in request.session:
+        request.session["tasks"] = []
     return render(request, "LetsDoIt/index.html", {
-        "tasks": tasks
+        "tasks": request.session["tasks"]
     })
 
 def addtask(request):
-    return render(request, "LetsDoIt/add.html")
+    if request.method == 'POST':
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            task = data["task"]
+            desc = data["description"]
+            # tasks = request.session["tasks"]
+            # tasks.append((task.capitalize(),desc.capitalize())) 
+            task = (task.capitalize(), desc.capitalize())
+            request.session["tasks"] += [task]
+            return HttpResponseRedirect(reverse("LetsDoIt:index"))
+        else:
+            return render(request, "LetsDoIt/add.html", {
+        "form": form
+    })
+        
+    return render(request, "LetsDoIt/add.html", {
+        "form": NewTaskForm()
+    })
